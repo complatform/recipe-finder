9251509c66e1060255e7364b497332d5f64e58a2
complatform <kalkidansol1623@gmail.com>
2024-11-19 21:10:10 +0300

    cganges

---
+++ src/components/Recipe/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+        <div class="flex mt-5 ml-5">
+
+        <i class="fa-solid fa-bars"></i>
+        <h1 class="font-bold text-gray-600">food <span class="font-bold text-green-600">Mood</span></h1>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+    </div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Recipe/Recipe.vue
@@ -0,0 +1,17 @@
+<template>
+ <Nav/>
+</template>
+
+<script>
+
+import Nav from '@/components/Recipe/Nav.vue';
+export default {
+components:{
+    Nav
+}
+}
+</script>
+
+<style>
+
+</style>
